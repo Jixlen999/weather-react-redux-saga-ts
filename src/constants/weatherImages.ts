117f5b89c71fd13967fae44c6d8b1d4e3d6bd616
Victoria Ivanyutenko <viktoriyaivanyutenko@yandex.ru>
2022-12-23 12:30:21 +0300

    refactor: download imgs

--- src/constants/weatherImages.ts
+++ src/constants/weatherImages.ts
@@ -1,15 +1,11 @@
-const rainyBg =
-  'https://img.freepik.com/premium-photo/spring-rain-blooming-garden-concept-freshness-nature-weather-seasonal-background_548821-32855.jpg?w=2000';
-const rainyMain =
-  'https://www.metoffice.gov.uk/binaries/content/gallery/metofficegovuk/hero-images/weather/rain/rain-and-umbrella.jpg';
-const snowyBg = 'https://img1.goodfon.ru/wallpaper/nbig/3/d6/vetka-zima-sneg-hvoya.jpg';
-const snowyMain = 'https://images.wallpaperscraft.ru/image/single/gorod_vecher_snegopad_136478_1280x720.jpg';
-const sunnyBg =
-  'https://images.unsplash.com/photo-1511836536898-6d6f1b8f6948?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8c3VubnklMjBiYWNrZ3JvdW5kfGVufDB8fDB8fA%3D%3D&w=1000&q=80';
-const sunnyMain = 'https://www.deutschland.de/sites/default/files/media/image/TdT_Naturwunder_In_Deutschland.jpg';
-const overcastBg =
-  'https://cloudfront.slrlounge.com/wp-content/uploads/2019/01/%E2%80%9CShadows-of-the-Past%E2%80%9D.jpg';
-const overcastMain = 'https://www.wallpaperflare.com/static/760/211/828/dark-sky-dirt-road-landscape-wallpaper.jpg';
+import rainyBg from '@src/assets/rainyBg.jpg';
+import rainyMain from '@src/assets/rainyMain.jpg';
+import snowyBg from '@src/assets/snowyBg.jpg';
+import snowyMain from '@src/assets/snowyMain.jpg';
+import sunnyBg from '@src/assets/sunnyBg.jpg';
+import sunnyMain from '@src/assets/sunnyMain.jpg';
+import overcastBg from '@src/assets/overcastBg.jpg';
+import overcastMain from '@src/assets/overcastMain.jpg';
 
 const WeatherImages = {
   rain: {
